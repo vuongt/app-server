0b031221e03b8085cfb2a9f6ae1bb280ffae7440
tuyet <thi.vuong@student.ecp.fr>
2016-11-20 14:30:17 +0100

    cli server uncomment

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
-/*if (PHP_SAPI == 'cli-server') {
+if (PHP_SAPI == 'cli-server') {
     // To help the built-in PHP dev server, check if the request was actually for
     // something which should probably be served as a static file
     $url  = parse_url($_SERVER['REQUEST_URI']);
@@ -9,7 +9,7 @@ use \Psr\Http\Message\ResponseInterface as Response;
     if (is_file($file)) {
         return false;
     }
-}*/
+}
 require __DIR__ . '/../vendor/autoload.php';
 date_default_timezone_set('UTC');
 
